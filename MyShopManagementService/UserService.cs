e2edfac499dbb26d6048b409774f907dc492b1a9
Linh <khanhlinhyb03@gmail.com>
2025-04-25 16:45:31 +0700

    Initial commit

---
+++ MyShopManagementService/UserService.cs
@@ -0,0 +1,34 @@
+﻿using MyShopManagementBO;
+using MyShopManagementRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopManagementService
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserService() {
+            _userRepository = new UserRepository();
+            _unitOfWork = new UnitOfWork();
+        }
+        public User Get(string email)
+        {
+            return _unitOfWork.UserRepository.FindUserByName(email);
+        }
+
+
+
+        public List<User> GetAll() => _userRepository.GetAll();
+        public List<Role> GetRoles() => _userRepository.GetRoles();
+        public User Get(string email) => _userRepository.Get(email);
+        public void Create(User entity) => _userRepository.Create(entity);
+        public void Update(User entity) => _userRepository.Update(entity);
+        public void Delete(string email) => _userRepository.Delete(email);
+        public bool Exist(string email) => _userRepository.Exist(email);
+    }
+}
